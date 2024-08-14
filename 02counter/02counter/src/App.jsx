abb9a60ea6120bfa84c61a76b7c5802caf5dcbda
nishantyadav24 <ynishant567@gmail.com>
2024-08-14 18:33:30 +0530

    usestate and hooks

---
+++ 02counter/02counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+
+  let  [counter , setCounter]= useState(15) //use state varibale gives us  ararya with two values begineer  and a function
+// let counter = 15
+let addvalue = ()=>{
+  console.log("clicekd",counter)
+  setCounter(counter+1) // counter me kya daluna
+  counter = counter + 1;
+
+}
+  return (
+    <>
+    <h1> chai aur react </h1>
+    <h2> Counter value:{counter} </h2>
+    <button onClick={addvalue} >  Add value{counter} </button>
+    <br></br>
+    <button> remove value{counter} </button>
+
+    </>
+  )
+}
+
+export default App
