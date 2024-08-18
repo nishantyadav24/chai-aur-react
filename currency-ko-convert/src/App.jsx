b6d006d60f4c1690429dd3590e7c5950f898ba9c
nishantyadav24 <ynishant567@gmail.com>
2024-08-18 18:35:19 +0530

    refactoring app.jsx

--- currency-ko-convert/src/App.jsx
+++ currency-ko-convert/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
-
+import { Inputbox } from './components'
+import useCurrencyInfo from './hooks/usecurrencyinfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState("")
+const CurrencyInfo = useCurrencyInfo(from)
+const options = object.keys(CurrencyInfo)
   return (
     <>
  <h1 className='bg-orange-500 flex justify-center my-10 text-3xl'>Currency ko karein  convert </h1>
--- currency-ko-convert/src/components/Inputbox.jsx
+++ currency-ko-convert/src/components/Inputbox.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useId } from 'react';
 
-function Inputbox() {
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectedCurrency = "usd",
+  amountDisabled = false,
+  currencyDisabled = false,
+  className = "",
+}) {
+  const amountInputId = useId();
+
   return (
-    <div>Inputbox</div>
-  )
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className='w-1/2'>
+        <label htmlFor={amountInputId} className='text-black/40 mb-2 inline-block'>
+          {label}
+        </label>
+        <input
+          id={amountInputId}
+          className='outline-none w-full bg-transparent py-1.5'
+          type='number'
+          placeholder='Amount'
+          disabled={amountDisabled}
+          value={amount}
+          onChange={(e) => onAmountChange && onAmountChange(e.target.value)}
+        />
+      </div>
+      <div className='w-1/2 flex flex-wrap justify-end text-right'>
+        <label className='text-black/40 mb-2 inline-block'>
+          Currency
+        </label>
+        <select
+          className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+          value={selectedCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisabled}
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 }
 
-export default Inputbox
+export default InputBox;
